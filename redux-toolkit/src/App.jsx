d17148dbb7ac369aa90f371edfb61e881eb8310e
Bilal Irfan <bi0755562@gmail.com>
2024-11-15 21:39:05 +0500

    App.jsx & todoSlice.js Updated

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,8 +1,25 @@
 import './App.css'
 import AddTodo from './components/AddTodo'
 import Todo from './components/Todo'
+import { useSelector, useDispatch } from 'react-redux'
+import {useEffect} from 'react'
+import { setTodo } from './features/todo/todoSlice'
+
 
 function App() {
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const todo = JSON.parse(localStorage.getItem("todos"))
+    if(todo){
+      dispatch(setTodo(todo))
+    }
+  }, [dispatch])
+
+  useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <>
--- redux-toolkit/src/features/todo/todoSlice.js
+++ redux-toolkit/src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{id: 1, text: "Hello World"}]
+    todos: [{ id: 1, text: "Hello World" }]
 }
 
 export const todoSlice = createSlice({
@@ -13,14 +13,17 @@ export const todoSlice = createSlice({
                 id: nanoid(),
                 text: action.payload
             }
-            state.todos.push(todo)
+            state.todos.push(todo);
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload) 
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
         },
+        setTodo: (state, action) => {
+            state.todos = action.payload
+        }
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const { addTodo, removeTodo, setTodo } = todoSlice.actions;
 
-export default todoSlice.reducer
+export default todoSlice.reducer;
